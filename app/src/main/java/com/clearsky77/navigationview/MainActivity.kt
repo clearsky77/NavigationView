b15a09a11d9bf14ccd4eba3741421f2a447cb5ad
clearsky77 <clearsky3264@gmail.com>
2021-10-20 21:04:27 +0900

    네비게이션 메뉴(drawer) 열려 있을 때 -> 백버튼 눌러도 앱 종료되지 않게  - onBackPressed를 오버라이딩 해서 처리

--- app/src/main/java/com/clearsky77/navigationview/MainActivity.kt
+++ app/src/main/java/com/clearsky77/navigationview/MainActivity.kt
@@ -32,4 +32,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         layout_drawer.closeDrawers() // 선택이 끝났으니 네비게이션 메뉴 창은 닫아줘야한다.
         return false
     }
+
+//  백버튼(삼각형)을 클릭하였을 때 (드로워가 열린 상태에서 백버튼을 누르면 어플이 종료되는 현상 방지책)
+    override fun onBackPressed() { // Ctrl + O : 추천 오버라이딩 창이 뜬다.
+        if(layout_drawer.isDrawerOpen(GravityCompat.START)) { // 레이아웃 드로워가 열려있는가?
+            layout_drawer.closeDrawers() // 레이아웃 드로워를 닫아줘라.
+        }else{
+            super.onBackPressed() // 기존 백버튼 기능 실행 (finish)
+        }
+    }
 }
